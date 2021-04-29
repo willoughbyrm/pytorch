330ac75e1736f22c4414c0e0625bfe39d8010785
chenlai <chenlai@devvm2090.vll0.facebook.com>
2021-04-29 11:51:08 -0700

    [PyTorch][Edge] Add backport_toversion to export expected version model
    Add api `back_to_version(model_vn, version=i) -> model_vi`. Under the hood, add iteration to `backport(model_vn) -> model_vn-1`
    
    ## Test
    ```
    python test/mobile/test_bytecode.py
    ```
    Test with prod models (v5)
    
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/56855
    
    
    ghstack-source-id: 127760944
    
    Differential Revision: [D27984685](https://our.internmc.facebook.com/intern/diff/D27984685/)

--- torch/csrc/jit/mobile/backport.h
+++ torch/csrc/jit/mobile/backport.h
@@ -33,6 +33,31 @@ TORCH_API bool _backport_for_mobile(
     std::shared_ptr<caffe2::serialize::ReadAdapterInterface> rai,
     std::shared_ptr<caffe2::serialize::PyTorchStreamWriter> writer);
 
+TORCH_API bool _backport_for_mobile(
+    std::istream& in,
+    std::ostream& out,
+    const int64_t to_version);
+
+TORCH_API bool _backport_for_mobile(
+    std::istream& in,
+    const std::string& output_filename,
+    const int64_t to_version);
+
+TORCH_API bool _backport_for_mobile(
+    const std::string& input_filename,
+    std::ostream& out,
+    const int64_t to_version);
+
+TORCH_API bool _backport_for_mobile(
+    const std::string& input_filename,
+    const std::string& output_filename,
+    const int64_t to_version);
+
+TORCH_API bool _backport_for_mobile(
+    std::shared_ptr<caffe2::serialize::ReadAdapterInterface> rai,
+    std::unique_ptr<caffe2::serialize::PyTorchStreamWriter> writer,
+    const int64_t to_version);
+
 } // namespace mobile
 } // namespace jit
 } // namespace torch
