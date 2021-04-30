0ebab2e2985d5c48b2efd170dae06ce83cd413f7
wayi <wayi@devgpu227.prn2.facebook.com>
2021-04-30 00:15:10 -0700

    [RPC Framework] Enable RemoteModule to directly send GPU tensors over the wire on TensorPipe RPC backend if a device map is provided
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/57288
    
    If the device map provided by RemoteModue is not empty, then TensorPipe RPC backend can support directly sending GPU tensors over the wire.
    
    Also add pybind of `_get_device_map`.
    
    The changes in unit test setup is separated out as a follow-up PR, as currently it breaks some tests in `distributed/rpc/test_faulty_agent.py`.
    
    #Closes: https://github.com/pytorch/pytorch/issues/51670
    
    Differential Revision: [D28021672](https://our.internmc.facebook.com/intern/diff/D28021672/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D28021672/)!
    ghstack-source-id: 127822638

--- torch/csrc/distributed/rpc/rpc_agent.cpp
+++ torch/csrc/distributed/rpc/rpc_agent.cpp
@@ -288,7 +288,7 @@ bool RpcAgent::isGILProfilingEnabled() {
 }
 
 std::unordered_map<c10::DeviceIndex, c10::DeviceIndex> RpcAgent::getDeviceMap(
-    const WorkerInfo& dest) const {
+    const WorkerInfo& dst) const {
   // Default implementation has no device map.
   return {};
 }
